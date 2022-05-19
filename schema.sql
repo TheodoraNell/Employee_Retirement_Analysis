a47db8a6693b39d7ee68ae255f937638282c1593
Theodora Lopez <theodora.n.lopez@gmail.com>
2022-05-18 19:12:52 -0700

    Add retirement info.

--- schema.sql
+++ schema.sql
@@ -17,13 +17,23 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE Dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -31,9 +41,9 @@ CREATE TABLE salaries (
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no),
 );
+
 
 CREATE TABLE titles (
   emp_no INT NOT NULL,
@@ -41,17 +51,8 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, title, from_date)	
 );
 
-CREATE TABLE Dept_emp (
-  emp_no INT NOT NULL,
-  dept_no VARCHAR NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (emp_no)
-);
 
 SELECT * FROM departments;
